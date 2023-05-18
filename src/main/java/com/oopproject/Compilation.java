2412e542fbdf85e994bc8b29a4c94ecbf05680f7
frncsbcn <302418@studenti.unimore.it>
2023-05-18 11:10:14 +0200

    Added Library song management methods

--- src/main/java/com/oopproject/Compilation.java
+++ src/main/java/com/oopproject/Compilation.java
@@ -3,8 +3,13 @@ package com.oopproject;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public abstract class Compilation {
+    private String name;
     private CopyOnWriteArrayList<Integer> songs;
     public void addSong(int index)  {
+
+    }
+
+    public void removeSong(int index)   {
 
     }
 }
--- src/main/java/com/oopproject/Library.java
+++ src/main/java/com/oopproject/Library.java
@@ -9,50 +9,74 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 public class Library {
+    private Properties properties;
     private final ConcurrentHashMap<Integer, String> songs = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, Playlist> playlists = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, Album> albums = new ConcurrentHashMap<>();
 
     public Library(Properties props, boolean isFirstSetup) {
+        this.properties = props;
         File directory = new File(props.getProperty("libraryFolder"));
         CopyOnWriteArrayList<File> files = new CopyOnWriteArrayList<>(List.of(Objects.requireNonNull(directory.listFiles())));
 
         if (!isFirstSetup)  {
-            new Library(props, files);
+            new Library(files);
         }   else {
             files.stream()
                     .sorted()
                     .collect(Collectors.toCollection(() -> files));
 
             for (int i = 0; i < files.size(); ++i) {
-                addNewSong(files.get(i).getAbsolutePath(), i, props);
+                addNewSong(files.get(i).getAbsolutePath(), i);
             }
         }
     }
 
-    public Library(Properties props, CopyOnWriteArrayList<File> files) {
+    public Library(CopyOnWriteArrayList<File> files) {
         for (File file : files) {
             if (Song.getIndex(file.getAbsolutePath()).matches(""))  {
-                addNewSong(file.getAbsolutePath(), props.size(), props);
+                addNewSong(file.getAbsolutePath(), properties.size());
             }
 
-            String[] pls = props.getProperty(Song.getIndex(file.getAbsolutePath())).split("\n");
+            String[] pls = properties.getProperty(Song.getIndex(file.getAbsolutePath())).split("\n");
             for (String pl : pls)   {
                 playlists.putIfAbsent(pl, new Playlist(pl));
                 playlists.get(pl).addSong(Integer.parseInt(Song.getIndex(file.getAbsolutePath())));
             }
-
-            String album = new Media(file.getAbsolutePath()).getMetadata().get("album").toString();
-            if (!album.matches(""))    {
-                albums.putIfAbsent(album, new Album(album));
-                albums.get(album).addSong(Integer.parseInt(Song.getIndex(file.getAbsolutePath())));
-            }
         }
     }
 
-    public void addNewSong(String path, int index, Properties props)  {
+    public void addSongFile(String path)    {
+        File oldFile = new File(path);
+        File song = new File(properties.getProperty("libraryFolder"), path);
+        boolean delete = oldFile.delete();
+        addNewSong(song.getAbsolutePath(), properties.size());
+    }
+
+    public void addNewSong(String path, int index)  {
         Song.setIndex(path, index);
-        props.setProperty(String.valueOf(index), "");
+        properties.setProperty(String.valueOf(index), "");
         songs.put(index, path);
+        String album = new Media(path).getMetadata().get("album").toString();
+        if (!album.matches("")) {
+            albums.putIfAbsent(album, new Album(album));
+            albums.get(album).addSong(Integer.parseInt(Song.getIndex(path)));
+        }
+    }
+
+    public void deleteSong(int index)   {
+        for (String playlist : properties.getProperty(String.valueOf(index)).split("\n"))   {
+            playlists.get(playlist).removeSong(index);
+        }
+
+        String album = new Media(songs.get(index)).getMetadata().get("album").toString();
+        if (!album.matches("")) {
+            albums.get(album).removeSong(index);
+        }
+
+        File song = new File(songs.get(index));
+        boolean delete = song.delete();
+        songs.remove(index);
+        properties.remove(String.valueOf(index));
     }
 }
