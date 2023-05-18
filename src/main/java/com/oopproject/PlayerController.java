dc8b0e4b4caed3a0770c436276f2accdefcf2e80
rikiv <301114@studenti.unimore.it>
2023-05-18 19:42:16 +0200

    Added listener to timeSlider. Added currentTimeLabel and totalTimeLabel. Added method to manipulate time of the song. Added some test song to resources

--- src/main/java/com/oopproject/PlayerController.java
+++ src/main/java/com/oopproject/PlayerController.java
@@ -1,15 +1,17 @@
 package com.oopproject;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.*;
 import java.io.File;
-import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
@@ -19,6 +21,11 @@ public class PlayerController implements Initializable {
 
 
     @FXML Label songLabel;
+    @FXML Label currentTimeLabel;
+    @FXML Label totalTimeLabel;
+    @FXML Slider timeSlider;
+    @FXML Slider volumeSlider;
+
     private File directory;
     private File[] files;
     private ArrayList<File> songs;
@@ -29,18 +36,51 @@ public class PlayerController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         //Testing code Start
-        songs = new ArrayList<File>();
+        songs = new ArrayList<>();
         directory = new File("C:\\Users\\rikiv\\OneDrive\\Desktop\\MediaMusic");
         files = directory.listFiles();
         if (files != null){
             Collections.addAll(songs, files);
         }
+        volumeSlider.setMax(1.0);
+        volumeSlider.setValue(0.5);
+        mediaLoader();
+        //Testing code End
+    }
+    private void mediaLoader(){
         media = new Media(songs.get(songNumber).toURI().toString());
         mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setVolume(1);
+        mediaPlayer.volumeProperty().bindBidirectional(volumeSlider.valueProperty());
+        timeSlider.setValue(0.0);
+        currentTimeLabel.setText("0");
+
+
+        mediaPlayer.totalDurationProperty().addListener((observable, oldDuration, newDuration) -> {
+            timeSlider.setMax(newDuration.toSeconds());
+            totalTimeLabel.setText(getTime(newDuration));
+        });
+        timeSlider.valueChangingProperty().addListener((observable, wasChanging, isChanging) -> {
+            if(!isChanging){
+                mediaPlayer.seek(Duration.seconds(timeSlider.getValue()));
+                currentTimeLabel.setText(getTime(mediaPlayer.getCurrentTime()));
+            }
+        });
+        timeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            double currentTime = mediaPlayer.getCurrentTime().toSeconds();
+            if (Math.abs(currentTime - newValue.doubleValue()) > 0.5){
+                mediaPlayer.seek(Duration.seconds(newValue.doubleValue()));
+                currentTimeLabel.setText(getTime(Duration.seconds(newValue.doubleValue())));
+            }
+        });
+        mediaPlayer.currentTimeProperty().addListener((observable, oldDuration, newDuration) -> {
+            if (!timeSlider.isValueChanging()){
+                timeSlider.setValue(newDuration.toSeconds());
+                currentTimeLabel.setText(getTime(newDuration));
+            }
+        });
         mediaPlayer.setOnEndOfMedia(this::nextAction);
         songLabel.setText(songs.get(songNumber).getName());
-        //Testing code End
+
     }
     @FXML void nextAction() {
         if(songNumber == songs.size() - 1){
@@ -49,10 +89,7 @@ public class PlayerController implements Initializable {
             songNumber++;
         }
         stopAction();
-        media = new Media(songs.get(songNumber).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        songLabel.setText(songs.get(songNumber).getName());
-        mediaPlayer.setOnEndOfMedia(this::nextAction);
+        mediaLoader();
         startAction();
     }
 
@@ -63,10 +100,7 @@ public class PlayerController implements Initializable {
             songNumber--;
         }
         stopAction();
-        media = new Media(songs.get(songNumber).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        songLabel.setText(songs.get(songNumber).getName());
-        mediaPlayer.setOnEndOfMedia(this::nextAction);
+        mediaLoader();
         startAction();
     }
 
@@ -91,10 +125,30 @@ public class PlayerController implements Initializable {
     private void stopAction(){
         mediaPlayer.pause();
     }
+    public String getTime(Duration time){
+        int minutes = (int) time.toMinutes();
+        int seconds = (int) time.toSeconds();
+
+        if (seconds > 59){
+            seconds = seconds % 60;
+        }
+        if(minutes > 59){
+            minutes = minutes % 60;
+        }
+        if(minutes > 0){
+            return String.format("%02d:%02d", minutes, seconds);
+        } else {
+            return String.format("%02d", seconds);
+        }
+    }
+
+   //Debug methods
     @FXML void infoAction() {
         System.out.println(mediaPlayer.getStatus());
         System.out.println(media.getDuration());
         System.out.println(mediaPlayer.getCurrentTime());
+        System.out.println(mediaPlayer.getStopTime());
+
     }
 
     @FXML void debugAction(){
